3b3a48e14ab02c8def3b6d4d1b6a7d1dc7ca61e6
kesavan6442 <kesavan.mcse@gmail.com>
2026-03-22 19:13:14 +0530

    Add files via upload

---
+++ random_forest.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
+
+data = pd.read_csv("data/energydata_complete.csv")
+data = data.select_dtypes(include=['number']).dropna()
+
+X = data.drop("Appliances", axis=1)
+y = data["Appliances"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+print("Random Forest")
+print("MAE:", mean_absolute_error(y_test, pred))
+print("R2:", r2_score(y_test, pred))
